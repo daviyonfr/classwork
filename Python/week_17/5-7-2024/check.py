6e5a166165a7fa81382a0e6dd3eb8ca2e6e98a86
daviyon <dhoulder@bostonk12.org>
2024-05-08 09:26:19 -0400

    list practice

---
+++ Python/week_17/5-7-2024/check.py
@@ -0,0 +1,25 @@
+#daviyon houlder
+#5/7/2024
+
+#creating an empty list
+a = []
+
+#using the operater to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+#printing the list
+print(a)
+
+#creating an empty list named b
+b =[]
+
+#using the function to add to the list b
+b.appened(1)
+b.appened(2)
+b.appened(3)
+
+#printing the list b
+print(b)
+
