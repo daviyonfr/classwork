4bb9f7d48a37f82d1cdd30aeb6828fea93d46c5e
392550 <dhoulder@bostonk12.org>
2025-12-12 09:14:17 -0500

    12-12-25

---
+++ 2023-2024/Python/ Week_13/Competency/programa_dos/ archivo_cuatro.py
@@ -0,0 +1,77 @@
+#my_list = []
+#print(my_list)
+#fruits = ["apple", "banana", "orange"]
+#fruits = list(("apple", "orange", "banana"))
+#fruits.append("mango")
+#fruits.insert(2, "kiwi")
+#print(fruits)
+#print(fruits[0])
+#print(fruits[-1])
+#print(fruits[2])
+#fruits.remove("orange")
+#print(fruits)
+#fruits.pop(1)
+#print(fruits)
+from os import name
+
+#numbers = [10,20,30,40,50]
+#print(numbers[:3])
+#print(numbers[3:5])
+#print(numbers[1:-2])
+#print(30 in numbers)
+#print(100 in numbers)
+#print(len(numbers))
+
+#colors = ["red","blue"]
+#morecolors = ["green","yellow"]
+#print(colors)
+#print(more_colors)
+#colors.extend(more_colors)
+#print(colors)
+
+#scores = [85,92,78,90,88]
+#print(scores)
+#scores.sort()
+#print(scores)
+#scores.reverse()
+#print(scores)
+#reversed_scores=scores[::-1]
+#print(reversed_scores)
+
+#letters = ["a", "b", "a", "c", "a", "b"]
+#letters.count("")
+#print(letters)
+#print(letters.count("a"))
+
+#fruits = ["apple", "banana", "orange"]
+#fruits.clear()
+#print(fruits)
+
+#my_five_movies = ["godzilla_v_king_kong","shooting_stars","Jurassic_World_Rebirth","Spider-Man_Across_The_Spider-Verse","CreedIII"]
+#print(my_five_movies)
+#print(len(my_five_movies))
+#my_five_movies.append("Rampage")
+#print(my_five_movies)
+#my_five_movies.remove("Jurassic World_Rebirth")
+#print(my_five_movies)
+
+my_dict = {}
+print(my_dict)
+student = {
+    'name': "Alice",
+    'age' : 20,
+    'major':"computer science"            }
+print(student)
+print(student['name'])
+print(student['age'])
+print(student.keys("",""))
+#setText("text_area2"       ,
+ #setText(labelname, "sorry to hear that");
+#def is_even(number):
+print([1,2,3] + [4,5,])
+ #   return number % 2 == 0
+
+# Examples
+#print(is_even(6))
+#print(is_even(3))
+bool("")
---
+++ 2025-2026/python/lists.py
@@ -0,0 +1,19 @@
+# Daviyon Houlder
+# 12/12/2025
+
+def main():
+  w = [1, 2, 3, 4, 5]
+  w.append(6)
+  print(w)
+if __name__ == '__main__':
+    main()
+    #how to create a new empty list
+    a=[]
+    b=list()
+    #How to create a list with items
+    a = [1,2,3,4,5,6,7,8,9]
+    #how to find out how many items are in list
+    print(len(a))
+    print(a)
+    print("a")
+    print(a [1])
